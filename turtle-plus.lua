1840b7b627ece22e8062e36e6db571e92e23681f
Matthew Batchelder <borkweb@gmail.com>
2013-02-08 07:43:51 -0500

    Adding a turtle extension script

---
+++ turtle-plus.lua
@@ -0,0 +1,53 @@
+-- moves in the specified direction "times" times.  If movement
+-- is obstructed, the turtle will pause then try again
+function turtle.move( dir, times )
+	for i=1,times,1 do
+		while not turtle[dir]() do
+			sleep(0.1)
+		end
+	end
+end
+
+-- attempts to place items from a collection of slots into
+-- the target_slot if it is empty
+function turtle.distribute( target_slot, slots )
+	local num = 0
+
+	num = turtle.getItemCount( target_slot )
+
+	-- if the target_slot already has items, we don't need to distribute.
+	if num > 0 then
+		return num
+	end
+
+	-- loop over a collection of slots to locate stacks of > 1 to
+	-- distribute the items to the target_slot
+	for key,slot in pairs( slots ) do
+		if not ( target_slot == slot.id ) then
+			num = turtle.getItemCount( slot.id )
+
+			if num > 1 then
+				turtle.select( slot.id )
+				turtle.transferTo( target_slot, 1 )
+				return 1
+			end
+		end
+	end
+end
+
+-- loads items into a slot "how" you want it to. Returns
+-- the number of items loaded
+function turtle.load( slot, how )
+	local num = 0
+
+	turtle.select( slot )
+
+	num = turtle.getItemCount( slot )
+
+	if 0 == contains then
+		turtle[how]()
+		num = turtle.getItemCount( slot )
+	end
+	
+	return num
+end
